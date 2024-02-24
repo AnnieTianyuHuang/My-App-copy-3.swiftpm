b90334e8ae51c3e47367ef0e71f1c23027346c35
AnnieHuang <annietianyuhuang@berkeley.edu>
2024-02-23 19:03:16 -0500

    Initial Commit

---
+++ Change1View.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct Change1View: View {
+    @EnvironmentObject var selection: Selection
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.3).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                Text("Now that we've tackled typical heights, why not aim higher? Experience the thrill of skydiving at 100km, where the sky meets space.")
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .padding()
+                
+                Button("Replay Prev") {
+                    selection.value = 1
+                }
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.gray)
+                .cornerRadius(10)
+                
+                Button("Try Higher") {
+                    selection.value = 2
+                }
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue) // Different background color for distinction
+                .cornerRadius(10)
+            }
+        }
+    }
+}
+
+// SwiftUI previews are used for development purposes in Xcode and require a struct that conforms to the PreviewProvider protocol.
+
---
+++ EndView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct EndView: View {
+    @EnvironmentObject var selection: Selection
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.3).edgesIgnoringSafeArea(.all)
+            Text("Each cloud passed increases drag, making it harder to control the descent. Overwhelmed by the fluff, the parachute fails to deploy in time. NOW KEEP TRYING, AND ENJOY!")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            Button("Replay") {
+                self.selection.value = 4
+            }
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
+            }
+    }
+}
+            
+
